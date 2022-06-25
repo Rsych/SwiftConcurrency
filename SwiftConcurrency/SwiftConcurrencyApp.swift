484b904ba0a6d84b6ef899b6363df110f96a5f67
J. W. Kim <kim.jongwon@orioninc.com>
2022-06-25 16:53:50 +0300

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Ryan J. W. Kim on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
